b95bdb077590ff90892332fc2c7dcf27d9ff564a
eblondel <emmanuel.blondel1@gmail.com>
2024-11-15 02:13:41 +0100

    active vessels by strata

--- R/artfish_compute_effort_estimate.R
+++ R/artfish_compute_effort_estimate.R
@@ -10,14 +10,21 @@ compute_effort_estimate = function(
     active_vessels, effort, effort_source, active_days, minor_strata = NULL
 ){
   
+  strata = c("year", "month", "fishing_unit")
+  if(!is.null(minor_strata)) strata = c(strata, minor_strata)
+  
   #complete active days by eventually filling missing or zero values for 'effort_fishable_duration'
   active_days = complete_active_days(active_days)
   
   #compute effort activity coefficient
   AC = compute_effort_activity_coefficient(effort = effort, effort_source = effort_source, minor_strata = minor_strata)
   
+  active_vessels_by_strata = active_vessels %>%
+    dplyr::group_by_at(strata) %>%
+    dplyr::summarize(fleet_engagement_number = sum(fleet_engagement_number))
+  
   dt = AC %>% 
-    dplyr::left_join(y = active_vessels) %>% 
+    dplyr::left_join(y = active_vessels_by_strata) %>% 
     dplyr::left_join(y = active_days)
   dt$effort_estimate = dt$fleet_engagement_number * dt$effort_fishable_duration * dt$effort_activity_coefficient
   
