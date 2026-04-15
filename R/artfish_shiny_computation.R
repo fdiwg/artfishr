5b1d463f9dd0a0b4d4cada520d419bdf64ca1b75
eblondel <emmanuel.blondel1@gmail.com>
2026-04-16 00:10:26 +0200

    test computation shiny server

---
+++ R/artfish_shiny_computation.R
@@ -0,0 +1,62 @@
+#' @name artfish_shiny_computation_server
+#' @title ARTFISH Computation - Shiny Server Module
+#' @description
+#' Server-side shiny logic to run Artfish computation as business service
+#'
+#' @param id Character string. Module namespace identifier.
+#' @param refresh refresher for computation
+#' @param effort effort data
+#' @param effort_source Character string indicating the type of effort source.
+#' @param active_vessels active vessels data
+#' @param active_vessels_strategy active vessels strategy
+#' @param active_days active days
+#' @param landings landings
+#' @param minor_strata Character string targeting a column name considered as minor strata.
+#' @param progress_fn A function to monitor progress
+#'
+#' @export
+artfish_shiny_computation_server <- function(
+    id,
+    refresh,
+    effort, effort_source, 
+    active_vessels, active_vessels_strategy,
+    active_days,
+    landings,
+    minor_strata = NULL,
+    progress_fn = NULL
+) {
+  moduleServer(id, function(input, output, session) {
+    
+    estimates <- reactive({
+      refresh()
+      
+      minor_strata_val <- if (is.null(minor_strata)) NULL else minor_strata()
+      
+      isolate({
+        artfishr::compute_report(
+          effort = effort(),
+          effort_source = effort_source(),
+          active_vessels = active_vessels(),
+          active_vessels_strategy = active_vessels_strategy(),
+          active_days = active_days(),
+          landings = landings(),
+          minor_strata = minor_strata_val,
+          progress_fn = progress_fn
+        )
+      })
+    }) |>
+      bindCache(list(
+        effort(),
+        effort_source(),
+        active_vessels(),
+        active_vessels_strategy(),
+        active_days(),
+        landings(),
+        if (!is.null(minor_strata)) minor_strata_val
+      ))
+    
+    list(
+      estimates = estimates
+    )
+  })
+}
